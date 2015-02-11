82f962be77ad954f1314e5351c5263e6f991879d
Christopher De Cairos <cade@mozillafoundation.org>
2015-02-11 17:44:09 -0500

    make proxy - broken due to relative urls

--- firehose/components/views/MakePreview.jsx
+++ firehose/components/views/MakePreview.jsx
@@ -2,28 +2,17 @@ var React = require("react");
 
 var MakePreview = React.createClass({
   render: function() {
-    var elem,
-        url = this.props.url,
+    var url = this.props.url,
         https = /^https/;
 
-    // If either of these conditions are true, mixed content won't be an issue
-    if ( !https.test(window.location) || https.test(url) ) {
-      elem = (
+      // proxy http urls
+      if ( !https.test(url) ) {
+        url = `/api/1.0/proxy-make/?url=${url}`;
+      }
+
+      return (
         <iframe src={url} className="make-preview"></iframe>
       );
-    } else {
-      elem = (
-        <div className="container col-md-12 text-center">
-          <a
-            className="make-preview-link"
-            href={this.props.url}
-            target="_blank"
-            title="http links cannot be loaded by iframe due to mixed content blocking">Open Make in Tab
-          </a>
-        </div>
-      );
-    }
-    return elem;
   }
 });
 
